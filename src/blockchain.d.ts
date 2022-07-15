0911b07e65b5f7d3cd19ef4dd48fb8493cc6d308
rqlalvs <raquelalves3753@gmail.com>
2022-07-14 22:39:53 -0300

    update types

--- src/blockchain.d.ts
+++ src/blockchain.d.ts
@@ -1,2 +1,34 @@
-declare module 'node_modules/my-cryptochain.js/src/blockchain';
-declare module 'node_modules/elliptic';
+export class BlockChain {
+    chain: Block[];
+    difficulty: number;
+    pendingTransactions: any[];
+    miningRewards: number;
+    createGenesisBlock(): Block;
+    getLatestBlock(): Block;
+    minePendingTransactions(miningRewardAddress: any): void;
+    addTransaction(transaction: any): void;
+    getBalanceOfAddress(address: any): number;
+    isChainValid(): boolean;
+}
+export class Transaction {
+    constructor(fromAddress: any, toAddress: any, amount: any);
+    fromAddress: any;
+    toAddress: any;
+    amount: any;
+    calculateHash(): any;
+    signTransaction(signingKey: any): void;
+    signature: any;
+    isValid(): any;
+}
+declare class Block {
+    constructor(timestamp: any, transactions: any, previousHash?: string);
+    timestamp: any;
+    transactions: any;
+    previousHash: string;
+    hash: any;
+    nonce: number;
+    calculateHash(): any;
+    mineBlock(difficulty: any): void;
+    hasValidTransaction(): boolean;
+}
+export {};
